1377683b374666765f8da3965ba14520954d1ee6
snowtoslow <snowtoslow@mail.ru>
2020-08-13 22:16:40 +0300

    add new condition

--- WeirdString/WeirdString.go
+++ WeirdString/WeirdString.go
@@ -2,33 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"unicode"
 )
 
 func main() {
 
-	myString := "Weird string"
+
+	myString := "This is a test Looks like you passed"
 
 	fmt.Println(weirdCase(myString))
-
-	fmt.Println(test(weirdCase(myString),"WeIrD StRiNg"))
-
-
 }
 
 func weirdCase(inputString string) string{
 	myRune:= []rune(inputString)
 	for i:=0;i<len(myRune);i++{
 		if i%2==0 {
+			log.Printf("IF:%d--->%s",i,string( unicode.ToUpper(myRune[i])))
+			if myRune[i]==32 {
+				log.Print("cont")
+				continue
+			}
+
+			myRune[i] =  unicode.ToUpper(myRune[i])
+		}else if i%2!=0{
+			log.Printf("ELSE IF:%d--->%s",i,string(unicode.ToLower(myRune[i])))
 			if myRune[i]==32 {
 				continue
 			}
-			myRune[i] =  unicode.ToUpper(myRune[i])
+			myRune[i] =  unicode.ToLower(myRune[i])
 		}
 	}
 	return string(myRune)
-}
-
-func test(s string, r string) bool{
-	return s == r
 }
