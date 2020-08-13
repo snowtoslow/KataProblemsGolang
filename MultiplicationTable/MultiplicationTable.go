8a9a5b454ab32e5813284e28fb0c1e9ba5fbe000
snowtoslow <snowtoslow@mail.ru>
2020-08-13 21:34:35 +0300

    add all

--- MultiplicationTable/MultiplicationTable.go
+++ MultiplicationTable/MultiplicationTable.go
@@ -1,5 +1,21 @@
 package main
 
 func main() {
-	$END$
+
+	multiplicationTable(1)
+
+}
+
+func multiplicationTable(size int) [][]int {
+	myArray := make([][]int, size)
+	for i:= range myArray{
+		myArray[i] = make([]int, size)
+		vector := make([]int, size)
+		for j := range myArray[i]{
+			vector[j] = (i+1)*(j+1)
+			myArray[i][j] = vector[j]
+		}
+	}
+	return myArray
+
 }
--- WeirdString/WeirdString.go
+++ WeirdString/WeirdString.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"unicode"
+)
+
 func main() {
 
+	myString := "Weird string"
+
+	fmt.Println(weirdCase(myString))
+
+	fmt.Println(test(weirdCase(myString),"WeIrD StRiNg"))
+
+
+}
+
+func weirdCase(inputString string) string{
+	myRune:= []rune(inputString)
+	for i:=0;i<len(myRune);i++{
+		if i%2==0 {
+			if myRune[i]==32 {
+				continue
+			}
+			myRune[i] =  unicode.ToUpper(myRune[i])
+		}
+	}
+	return string(myRune)
+}
+
+func test(s string, r string) bool{
+	return s == r
 }
