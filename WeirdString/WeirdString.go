b9916a911fe20ce7dc994d877d88d515c8d3d655
snowtoslow <snowtoslow@mail.ru>
2020-08-17 22:23:29 +0300

    delete logs

--- WeirdString/WeirdString.go
+++ WeirdString/WeirdString.go
@@ -18,13 +18,10 @@ func weirdCase(inputString string) string{
 	myRune:= []rune(inputString)
 	for i:=0;i<len(myRune);i++{
 		if myRune[i]==32 {
-			log.Print("CONTINUE")
 			continue
 		}else if i%2==0{
-			log.Printf("EVEN:%d--->%s",i,string(unicode.ToUpper(myRune[i])))
 			myRune[i] =  unicode.ToUpper(myRune[i])
 		} else {
-			log.Printf("ODD:%d--->%s",i, string(unicode.ToLower(myRune[i])))
 			myRune[i] =  unicode.ToLower(myRune[i])
 		}
 	}
